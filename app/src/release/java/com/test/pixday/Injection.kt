596dd87d4207a721dfcb99531e25e9aa05ef5c44
Kehinde Ogunde <codekenn@gmail.com>
2021-07-27 06:38:39 +0100

    Adds Injection object for release variant

---
+++ app/src/release/java/com/test/pixday/Injection.kt
@@ -0,0 +1,18 @@
+package com.test.pixday
+
+import androidx.lifecycle.ViewModelProvider
+import com.test.pixday.api.ShutterStockService
+import com.test.pixday.data.ShutterStockRepository
+import com.test.pixday.ui.ViewModelFactory
+
+private const val accessToken = "SHUTTER_STOCK_RELEASE_ACCESS_TOKEN_HERE"
+
+object Injection {
+    private fun provideShutterStockRepository(): ShutterStockRepository {
+        return ShutterStockRepository(ShutterStockService.create(accessToken))
+    }
+
+    fun provideViewModelFactory(): ViewModelProvider.Factory {
+        return ViewModelFactory(provideShutterStockRepository())
+    }
+}
